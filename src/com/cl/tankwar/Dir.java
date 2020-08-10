4dd39c21a47b9908b4e688c0e99c964fb304b352
matt <16417560@qq.com>
2020-08-10 17:40:30 +0800

    学习完Git后第一次提交代码

---
+++ src/com/cl/tankwar/Dir.java
@@ -0,0 +1,14 @@
+package com.cl.tankwar;
+
+/**
+ * @Author: Matt Ten
+ * @Date: 2020/8/9 23:10
+ * @Description: com.cl.tankwar
+ * @Version: 1.0
+ */
+public enum Dir {
+    LEFT,
+    UP,
+    RIGHT,
+    DOWN
+}
---
+++ src/com/cl/tankwar/TankFrame.java
@@ -0,0 +1,127 @@
+//
+//                            _ooOoo_  
+//                           o8888888o  
+//                           88" . "88  
+//                           (| -_- |)  
+//                            O\ = /O  
+//                        ____/`---'\____  
+//                      .   ' \\| |// `.  
+//                       / \\||| : |||// \  
+//                     / _||||| -:- |||||- \  
+//                       | | \\\ - /// | |  
+//                     | \_| ''\---/'' | |  
+//                      \ .-\__ `-` ___/-. /  
+//                   ___`. .' /--.--\ `. . __  
+//                ."" '< `.___\_<|>_/___.' >'"".  
+//               | | : `- \`.;`\ _ /`;.`/ - ` : | |  
+//                 \ \ `-. \_ __\ /__ _/ .-` / /  
+//         ======`-.____`-.___\_____/___.-`____.-'======  
+//                            `=---='  
+//  
+//         .............................................  
+//                  佛祖镇楼                  BUG辟易  
+//          佛曰:  
+//                  写字楼里写字间，写字间里程序员；  
+//                  程序人员写程序，又拿程序换酒钱。  
+//                  酒醒只在网上坐，酒醉还来网下眠；  
+//                  酒醉酒醒日复日，网上网下年复年。  
+//                  但愿老死电脑间，不愿鞠躬老板前；  
+//                  奔驰宝马贵者趣，公交自行程序员。  
+//                  别人笑我忒疯癫，我笑自己命太贱；  
+//                  不见满街漂亮妹，哪个归得程序员。
+
+package com.cl.tankwar;
+
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+/**
+ * @Author: Matt Ten
+ * @Date: 2020/8/9 23:10
+ * @Description: com.cl.tankwar
+ * @Version: 1.0
+ */
+public class TankFrame extends Frame{
+    Tank myTank = new Tank(200, 200, Dir.DOWN);
+
+    public TankFrame() {
+        setSize(800, 600);
+        setResizable(false);
+        setTitle("Tank War");
+        setVisible(true);
+
+        this.addKeyListener(new MyKeyListener());
+
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        myTank.paint(g);
+
+    }
+
+    class MyKeyListener extends KeyAdapter {
+        boolean bL = false;
+        boolean bU = false;
+        boolean bR = false;
+        boolean bD = false;
+        @Override
+        public void keyPressed(KeyEvent e) {
+            int key = e.getKeyCode();
+            switch (key) {
+                case KeyEvent.VK_LEFT :
+                    bL = true;
+                    break;
+                case KeyEvent.VK_UP :
+                    bU = true;
+                    break;
+                case KeyEvent.VK_RIGHT :
+                    bR = true;
+                    break;
+                case KeyEvent.VK_DOWN :
+                    bD = true;
+                    break;
+                default:
+                    break;
+            }
+            setMainTankDir();
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+            int key = e.getKeyCode();
+            switch (key) {
+                case KeyEvent.VK_LEFT :
+                    bL = false;
+                    break;
+                case KeyEvent.VK_UP :
+                    bU = false;
+                    break;
+                case KeyEvent.VK_RIGHT :
+                    bR = false;
+                    break;
+                case KeyEvent.VK_DOWN :
+                    bD = false;
+                    break;
+                default:
+                    break;
+            }
+            setMainTankDir();
+        }
+        private void setMainTankDir() {
+            if (bL) {myTank.setDir(Dir.LEFT);}
+            if (bU) {myTank.setDir(Dir.UP);}
+            if (bR) {myTank.setDir(Dir.RIGHT);}
+            if (bD) {myTank.setDir(Dir.DOWN);}
+        }
+    }
+}
