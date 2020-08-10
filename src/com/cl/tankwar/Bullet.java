e0522a97e2baa5f63725c5378a792ea1cef777e2
matt <16417560@qq.com>
2020-08-11 00:54:48 +0800

    （上个版本没有将子弹类加入工作区，重新提交）定义子弹类，设定属性和方法，new出一颗新的子弹，在屏幕上画出来

---
+++ src/com/cl/tankwar/Bullet.java
@@ -0,0 +1,81 @@
+//
+//                            _ooOoo_  
+//                           o8888888o  
+//                           88" . "88  
+//                           (| -_- |)  
+//                            O\ = /O  
+//                        ____/`---'\____  
+//                      .   ' \\| |// `.  
+//                       / \\||| : |||// \  
+//                     / _||||| -:- |||||- \  
+//                       | | \\\ - /// | |  
+//                     | \_| ''\---/'' | |  
+//                      \ .-\__ `-` ___/-. /  
+//                   ___`. .' /--.--\ `. . __  
+//                ."" '< `.___\_<|>_/___.' >'"".  
+//               | | : `- \`.;`\ _ /`;.`/ - ` : | |  
+//                 \ \ `-. \_ __\ /__ _/ .-` / /  
+//         ======`-.____`-.___\_____/___.-`____.-'======  
+//                            `=---='  
+//  
+//         .............................................  
+//                  佛祖镇楼                  BUG辟易  
+//          佛曰:  
+//                  写字楼里写字间，写字间里程序员；  
+//                  程序人员写程序，又拿程序换酒钱。  
+//                  酒醒只在网上坐，酒醉还来网下眠；  
+//                  酒醉酒醒日复日，网上网下年复年。  
+//                  但愿老死电脑间，不愿鞠躬老板前；  
+//                  奔驰宝马贵者趣，公交自行程序员。  
+//                  别人笑我忒疯癫，我笑自己命太贱；  
+//                  不见满街漂亮妹，哪个归得程序员。
+
+package com.cl.tankwar;
+
+import java.awt.*;
+
+/**
+ * @Author: Matt Ten
+ * @Date: 2020/8/11 0:38
+ * @Description: com.cl.tankwar
+ * @Version: 1.0
+ */
+public class Bullet {
+    private static final int SPEED = 10;
+    private final static int WITDH = 30, HEIGHT = 30;
+    private int x, y;
+    private Dir dir;
+
+    public Bullet(int x, int y, Dir dir) {
+        this.x = x;
+        this.y = y;
+        this.dir = dir;
+    }
+
+    public void paint(Graphics g) {
+        Color c = g.getColor();
+        g.setColor(Color.RED);
+        g.fillOval(x, y,WITDH, HEIGHT);
+        g.setColor(c);
+        move();
+    }
+
+    private void move() {
+        switch (dir) {
+            case LEFT:
+                x -= SPEED;
+                break;
+            case UP:
+                y -= SPEED;
+                break;
+            case RIGHT:
+                x += SPEED;
+                break;
+            case DOWN:
+                y += SPEED;
+                break;
+            default:
+                break;
+        }
+    }
+}
